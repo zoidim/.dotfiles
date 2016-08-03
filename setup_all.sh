a84a686e212097c2e0ea26af59f4365178121fab
Stephen Wrathall <stephen.wrathall@agworld.com.au>
2016-08-03 16:23:01 +0800

    Get Jack's latest dotfiles scripts, and add Atom

--- setup_all.sh
+++ setup_all.sh
@@ -1,21 +1,28 @@
+#!/bin/bash
 pushd $HOME/.dotfiles
 
+source ./functions.sh
+source ./check_os.sh
+echo System is $OS
+
+echo 'Checking requirements...'
+command -v curl >/dev/null 2>&1 || { echo >&2 "  Please install curl to run this script."; exit 1; }
+command -v vim >/dev/null 2>&1 || { echo >&2 "  Please install vim to run this script."; exit 1; }
+command -v git >/dev/null 2>&1 || { echo >&2 "  Please install git to run this script."; exit 1; }
+echo '  OK'
+
 echo 'Linking all symlinks into homedir:'
-for i in `find \`pwd\` -iname '*.symlink'`; do
-  file=$(basename $i)
-  file=.${file%%.symlink}
-  homefile=$HOME/$file
-  if [ ! -L $homefile ]; then
-    echo "  $i -> $homefile"
-    ln -s -i $i $homefile
-  else
-    echo "  $homefile is already a symlink"
-  fi
+for file in `find \`pwd\` -iname '*.symlink'`; do
+  basefile=$(basename $file)
+  basefile=.${basefile%%.symlink}
+  homefile=$HOME/$basefile
+  safelink $file $homefile
 done
 
 echo 'Calling all auto setups:'
-for i in `find . -iname 'auto_setup.sh' -maxdepth 2`; do
-  echo "  ${i:2}"
+for i in `find . -maxdepth 2 -iname 'auto_setup.sh'`; do
+  echo
+  echo "• ${i:2}"
   bash $i
 done
 
